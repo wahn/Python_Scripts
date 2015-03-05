322782c8ebd58cdbcee7dedc9ad1d0e5c768a46a
Jan Walter <jan@janwalter.com>
2015-03-05 23:38:06 +0100

    Use constructor to set Monday to be the first day of the week.

--- PySide/calendar.py
+++ PySide/calendar.py
@@ -3,10 +3,13 @@
 import sys
 
 import PySide
-from PySide.QtGui import QApplication, QCalendarWidget
+from PySide.QtCore import Qt
+from PySide.QtGui  import QApplication, QCalendarWidget
 
 class Window(QCalendarWidget):
-    pass
+    def __init__(self, parent = None):
+        super(Window, self).__init__(parent)
+        self.setFirstDayOfWeek(Qt.DayOfWeek.Monday)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
