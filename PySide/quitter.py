510ac931460a546139932a8a638bd0061a493c9e
Jan Walter <jan@janwalter.com>
2015-03-05 20:02:22 +0100

    Use Makefile to generate the ui_quitter.py file (from quitter.ui).

---
+++ PySide/quitter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# quitter.py - provide a button to quit this "program"
+
+import sys
+
+from PySide.QtGui import QMainWindow, QPushButton, QApplication
+
+from ui_quitter import Ui_MainWindow
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    frame = MainWindow()
+    frame.show()
+    app.exec_()
